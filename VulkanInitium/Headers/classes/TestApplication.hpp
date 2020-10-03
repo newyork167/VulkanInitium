c38d9a40e0aad094e3fb300eeb111dd62d1ffaa3
Newyork167 <newyork167@gmail.com>
2020-10-02 20:39:54 -0400

    Started Hello Triangle Code

--- VulkanInitium/Headers/classes/TestApplication.hpp
+++ VulkanInitium/Headers/classes/TestApplication.hpp
@@ -15,6 +15,7 @@ public:
 
 private:
 	GLFWwindow* window;
+	VkInstance instance;
 
 	void process_input(GLFWwindow* window) {
 		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
@@ -32,6 +33,30 @@ private:
 	}
 
 	void initVulkan() {
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+		createInfo.enabledLayerCount = 0;
+
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}
 
 	}
 
@@ -43,8 +68,8 @@ private:
 	}
 
 	void cleanup() {
+		vkDestroyInstance(instance, nullptr);
 		glfwDestroyWindow(window);
-
 		glfwTerminate();
 	}
 };
