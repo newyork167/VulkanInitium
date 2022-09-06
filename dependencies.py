a44e376a1232da04e033cbff832f5ee60b8e3cc6
Newyork167 <newyork167@gmail.com>
2022-09-06 19:40:16 -0400

    Updated file structure and started adding conan integration

---
+++ dependencies.py
@@ -0,0 +1,17 @@
+requires = (
+    ("assimp/5.0.1"),
+    ("fmt/9.0.0"),
+    ("bullet3/3.24"),
+    ("glew/2.1.0"),
+    ("glad/0.1.34"),
+    ("glfw/3.3.6"),
+    ("glm/0.9.9.8"),
+    ("stb/20200203"),
+    ("imgui/1.79"),
+    ("spdlog/1.10.0"),
+    ("boost/1.79.0")
+)
+
+options = '''
+  glew:shared=False
+'''
